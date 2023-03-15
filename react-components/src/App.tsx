10a2a172d2f54924378876681e60d0e4811221be
Levani <levani.imna@gmail.com>
2023-03-15 12:49:32 -0700

    init: start components task

---
+++ react-components/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class App extends React.Component {
+
+  render(): React.ReactNode {
+    return (
+      <div>
+        <h1>React. Components</h1>
+      </div>
+    )
+  }
+}
+
+export default App
