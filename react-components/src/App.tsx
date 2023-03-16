551039b15ccce179059a08dff2cd653df70accf4
Levani <levani.imna@gmail.com>
2023-03-16 12:12:41 -0700

    fix: add and configure dependencies

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 
 class App extends React.Component {
-
   render(): React.ReactNode {
     return (
       <div>
         <h1>React. Components</h1>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
